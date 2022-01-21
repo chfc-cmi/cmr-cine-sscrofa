5cba163345afb81d5f95cdcf2402d226af3e40df
Markus Ankenbrand <markus@ankenbrand.me>
2022-01-21 09:05:40 +0100

    code: script from annex to git

--- code/dcm_to_png.sh
+++ code/dcm_to_png.sh
@@ -1,1 +1,14 @@
-../.git/annex/objects/xG/19/MD5E-s399--f878f4b56c9ff4280c31a3d8d8815a96.sh/MD5E-s399--f878f4b56c9ff4280c31a3d8d8815a96.sh
+#!/bin/bash
+
+mkdir -p data/png/images
+for j in data/raw/images/*
+do
+	ID=$(basename $j)
+	echo $ID
+	SLICE=$(ls $j | cut -f1 -d"_" | sort -nr | head -n1)
+	for i in $(ls $j/*/*.dcm | sort -V)
+	do
+		out=$(echo $i | perl -ne '/(\d+)_BEATS/;printf "'$ID'_slice%03d",('$SLICE'-$1);/i(\d+).dcm/;printf "_frame%03d-image.png\n",($1-1)')
+		dcm2pnm --write-png --use-window 1 $i data/png/images/$out
+	done
+done
